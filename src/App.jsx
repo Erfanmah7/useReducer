7d809ba0fae00c0b9d38a101d3539c54f798e9c3
Erfanmah7 <efn8mah@yahoo.com>
2024-05-24 16:09:59 +0330

    Exercise the useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import HttpRes from "./HttpRes";
+import { useReducer } from "react";
+
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "Increase":
+      return { ...state, count: state.count + 1 };
+
+    default:
+      break;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
-      <HttpRes />
+      <div> {state.count}</div>
+      <button onClick={() => dispatch({ type: "Increase" })}>Increase</button>
+      <button onClick={() => dispatch("Increase By Amount")}>
+        Increase By Amount
+      </button>
+      <button onClick={() => dispatch("Reset")}>Reset</button>
+      <button onClick={() => dispatch("Decrease")}>Decrease</button>
     </>
   );
 }
